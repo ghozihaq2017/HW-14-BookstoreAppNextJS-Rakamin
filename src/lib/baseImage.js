08f49c5b84336d662f8e513d9537788af6013f6f
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-04-27 20:11:25 +0700

    fix: set .env base url

--- src/lib/baseImage.js
+++ src/lib/baseImage.js
@@ -1,3 +1,6 @@
-const BASE_IMAGE_URL = `http://localhost:3000`;
+const BASE_IMAGE_URL = env('BASE_IMAGE_URL');
+
+// kalo run lokal pake ini
+// const BASE_IMAGE_URL = `http://localhost:3000`;
 
 export default BASE_IMAGE_URL;
--- src/lib/baseUrl.js
+++ src/lib/baseUrl.js
@@ -1,3 +1,6 @@
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = env('BASE_URL');
+
+// kalo pake local ini
+// const BASE_URL = 'http://localhost:3000/api';
 
 export default BASE_URL;
